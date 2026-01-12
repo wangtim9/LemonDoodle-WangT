a16b43a1996370012221aec1fa0c298adfc83384
wangtim9 <wangtim27@7hills.org>
2026-01-12 09:38:51 -0500

    Add files via upload

---
+++ LemonadeStand.java
@@ -0,0 +1,10 @@
+import doodlepad.*;
+
+public class LemonadeStand {
+    public static void main(String[] args) {
+    
+        Rectangle r1 = new Rectangle(50, 60, 120, 80);
+        Rectangle r2 = new Rectangle(50, 60, 20, 100);
+        Rectangle r3 = new Rectangle(150, 60, 20, 100);
+    }
+}
